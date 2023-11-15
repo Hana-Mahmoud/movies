fb23537a7ee6de2bcf37164cc8a64c73034798df
Hana Mahmoud <hana.mokhtar@vodafone.com>
2023-11-15 15:38:53 +0100

    movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movies from './Component/Movies/Movies';
+import MovieList from './Component/MovieList/MovieList';
+import CreateMovie from './Component/CreateMovie/CreateMovie';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+     <Movies />
     </div>
   );
 }
