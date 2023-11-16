1512e4e65ecdfcf6b8c017553f5344e201cd361e
Hana Mahmoud <hana.mokhtar@vodafone.com>
2023-11-16 11:36:07 +0100

    movies

--- src/Component/CreateMovie/CreateMovie.tsx
+++ src/Component/CreateMovie/CreateMovie.tsx
@@ -19,42 +19,46 @@ return (
     <div className={Styles.form_container}>
       <h2>Add a New Movie</h2>
       <form onSubmit={handleMovieSubmit}>
-        <div className={Styles.form_container__input}>
-        <label  className={Styles.form_container__input__label}>Movie poster:</label>
+        <div className={Styles.form_container__field}>
+        <label  className={Styles.form_container__field__label}>Movie poster:</label>
         <input 
           type="text" 
+          className={Styles.form_container__field__input}
           required 
           value={stdImg}
           onChange={(e) => setImage(e.target.value)}
         />
         </div>
-        <div className={Styles.form_container__input}>
-        <label  className={Styles.form_container__input__label}>Movie title:</label>
+        <div className={Styles.form_container__field}>
+        <label  className={Styles.form_container__field__label}>Movie title:</label>
         <input 
           type="text" 
+          className={Styles.form_container__field__input}
           required 
          value={stdTitle}
          onChange={(e) => setTitle(e.target.value)}
         />
         </div>
-        <div className={Styles.form_container__input}>
-        <label  className={Styles.form_container__input__label}>Movie opening text:</label>
+        <div className={Styles.form_container__field}>
+        <label  className={Styles.form_container__field__label}>Movie opening text:</label>
         <textarea
           required
+          className={Styles.form_container__field__input}
          value={stdOpeningText}
          onChange={(e) => setText(e.target.value)}
         ></textarea>
         </div>
-        <div className={Styles.form_container__input}>
-        <label  className={Styles.form_container__input__label}>Movie release date:</label>
+        <div className={Styles.form_container__field}>
+        <label  className={Styles.form_container__field__label}>Movie release date:</label>
         <input 
           type="text" 
+          className={Styles.form_container__field__input}
           required 
          value={stdReleaseDate}
          onChange={(e) => setDate(e.target.value)}
         />
         </div>
-        <button>Add Movie</button>
+        <button className={Styles.form_container__button}>Add Movie</button>
       </form>
     </div>
     </>
